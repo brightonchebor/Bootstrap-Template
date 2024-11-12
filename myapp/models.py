cc55bf5f3a9137b0eff98a2d513bfc105f0247fa
brightonchebor <cheborbrighton805@gmail.com>
2024-11-12 15:52:07 +0300

    cn

--- myapp/models.py
+++ myapp/models.py
@@ -5,3 +5,7 @@ class Contact(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField()
     message = models.TextField()
+    
+    def __str__(self):
+        
+        return self.name
