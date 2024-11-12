e2a486b6448e72745ddc980f76b8cd66a4cf5338
brightonchebor <cheborbrighton805@gmail.com>
2024-11-12 16:23:31 +0300

    added phone and date

--- myapp/models.py
+++ myapp/models.py
@@ -5,7 +5,9 @@ class Contact(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField()
     message = models.TextField()
+    phone = models.IntegerField()
+    date = models.DateField()
     
     def __str__(self):
-        
+
         return self.name
