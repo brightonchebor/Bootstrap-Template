0490cbca81c859cd869c57241b4a790614994374
brightonchebor <cheborbrighton805@gmail.com>
2024-11-19 13:48:35 +0300

    cn

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 
 def register(request):
 
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        password_confirm = request.POST['password_confirm']
+
+        if password  == password_confirm:
+            try:
+                user = User.objects.create_user(
+                    username=username,
+                    password=password
+                )
+                user.save()
+                message = messages.success(request, 'Account created successfully')
+            except:
+                message = messages.error(request, 'User already exists')
+        else:
+            message = messages.error(request, 'Passwords did not match')
+
+    
+    
+
     return render(request, 'accounts/register.html', context={})
+
