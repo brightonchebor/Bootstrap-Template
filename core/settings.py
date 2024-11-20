c4ed926fda252486e60ee229cc2950e9fc03dff4
brightonchebor <cheborbrighton805@gmail.com>
2024-11-20 19:39:03 +0300

    cn

--- core/settings.py
+++ core/settings.py
@@ -80,8 +80,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangodb',
+        'USER': 'root',
+        'PASSWORD': 'sb62pu4142321',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
+
     }
 }
 
